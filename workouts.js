4e056b06946dab29d697da61a125239e4936a032
Gus <gusr@logicliving.com>
2020-05-30 10:24:02 -0500

    updates to app

---
+++ workouts.js
@@ -0,0 +1,19 @@
+var worksOuts = {
+	'bodyWeight': ['Jumping Jacks', 'High Knees', 'Burpees', 'Squates', 'Push-Ups'],
+
+	'upperBody': ['TRx Rows', 'Trx Y\'s', 'TRx I\'s', 'TRx T\'s'],
+
+	'lowerBody': ['Step-Ups with Weights', 'Weighted Lunges', 'Wall Sits']
+};
+
+var time = {
+
+	'20 Minutes': 20,
+	'30 Minutes': 30,
+	'40 Minutes': 40
+};
+
+
+
+
+module.exports = config; //allows this to be seen globally
